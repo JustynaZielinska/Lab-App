b802bcb358886dd0b755c73bd0a40b981c392b55
juzielska <juzielska@gmail.com>
2020-10-08 14:26:40 +0200

    Added animations in home-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,8 +17,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     PageNotFoundComponent
   ],
   imports: [
-    BrowserAnimationsModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
   ],
   providers: [],
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, style, animate, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
+  animations: [
+    trigger('isVisible', [
+      transition(':enter', [
+    query('.description span', [
+      style({opacity:0, transform: 'translateY(2rem'}),
+      stagger(800, [
+        animate('1s', style ({opacity:1, transform:'none'}))
+      ])
+    ])
+    ])
+    ]),],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+isVisible = false
 
-  constructor() { }
-
-  ngOnInit(): void {
+ngOnInit (): void{
+  this.isVisible = true
   }
-
 }
