e4cd0edcc3ae53f37b44eac03571c2de3308be64
juzielska <juzielska@gmail.com>
2021-01-12 20:24:53 +0100

    Problem with navigation refractor

--- src/app/navigation.service.ts
+++ src/app/navigation.service.ts
@@ -1,22 +1,56 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ICurrentPageData } from './navigation/InterfaceCurrentPageData';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NavigationService {
 
-  lastValidPage = new BehaviorSubject<null | 'gender' | 'test' | 'lipids' | 'thyroid'>(null);
+currentUrl: string;
+isValid: null | 'gender' | 'test' | 'lipids' | 'thyroid';
+currentPageData: ICurrentPageData;
+lastValidPage = new BehaviorSubject<null | 'gender' | 'test' | 'lipids' | 'thyroid'>(null);
+currentForm = new BehaviorSubject<null | 'gender-choice' | 'test-choice' | 'entering-results'>(null);
 
-  currentForm = new BehaviorSubject<null | 'gender-choice' | 'test-choice' | 'entering-results'>(null);
-
-  public changeIsValid(newValue: null | 'gender' | 'test' | 'lipids' | 'thyroid'): void {
-    this.lastValidPage.next(newValue);
-  }
-
-  public changeCurrentForm(newValue: null | 'gender-choice' | 'test-choice' | 'entering-results'): void {
-    this.currentForm.next(newValue);
-  }
+public changeIsValid(newValue: null | 'gender' | 'test' | 'lipids' | 'thyroid'): void {
+  this.lastValidPage.next(newValue);
 }
 
+public changeCurrentForm(newValue: null | 'gender-choice' | 'test-choice' | 'entering-results'): void {
+  this.currentForm.next(newValue);
+}
 
+changeNavigationProperties(currentForm, isValid): ICurrentPageData{
+  switch (currentForm) {
+    case 'test-choice':
+      let previousPath = 'home-page';
+      let previousForm = null;
+      let nextPath = 'test-form-page';
+      let nextForm = 'gender-choice';
+      let pageTitle = 'Wybierz płeć';
+      let isEnabled = this.changeIsEnabledProperty(isValid, 'test');
+      break;
+    case 'gender-choice':
+      previousPath = 'test-form-page';
+      previousForm = 'test-choice';
+      nextPath = 'test-form-page';
+      nextForm = 'entering-results';
+      pageTitle = 'Wpisz wyniki';
+      isEnabled = this.changeIsEnabledProperty(isValid, 'gender');
+      break;
+    case 'entering-results':
+      previousPath = 'test-form-page';
+      previousForm = 'gender-choice';
+      nextPath = 'interpretation';
+      pageTitle = 'Interpretacja';
+      isEnabled = this.changeIsEnabledProperty(isValid, 'thyroid', 'lipids');
+      return {previousPath, previousForm, nextPath, nextForm, pageTitle, isEnabled}; }
+}
+changeIsEnabledProperty(isValid, title: string, title2?: string): boolean{
+  if ((isValid === title ) || (isValid === title2)){
+    return true;
+  } else { return false; }
+}
+}
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,79 +1,56 @@
-import { Component, DoCheck, OnInit  } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { NavigationService } from 'src/app/navigation.service';
 import { Router } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { ICurrentPageData } from './InterfaceCurrentPageData';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements DoCheck, OnInit {
+export class NavigationComponent implements OnInit {
 
-constructor(public service: NavigationService, private currentRoute: Router) {}
+constructor(public navigationService: NavigationService, private router: Router) {}
 
+currentPageData: ICurrentPageData ;
 isEnabled: boolean;
 isHidden: boolean;
 isDisabled: boolean;
-pageTitle: 'Wybierz badanie' | 'Wybierz płeć' | 'Wpisz wyniki' | 'Interpretacja' | 'Wybierz badanie ponownie';
-previousPath: 'home-page' | 'test-form-page';
-nextPath: 'test-form-page' | 'interpretation';
-isValid: null | 'gender' | 'test' | 'lipids' | 'thyroid';
-previousForm: null | 'gender-choice' | 'test-choice' | 'entering-results';
-nextForm: null | 'gender-choice' | 'test-choice' | 'entering-results';
+currentUrl: string;
 currentForm: null | 'gender-choice' | 'test-choice' | 'entering-results';
+isValid: null | 'gender' | 'test' | 'lipids' | 'thyroid';
 
 goBack(): void {
-  this.service.changeIsValid(null);
+  this.navigationService.changeIsValid(null);
 }
 
 changeCurrentForm(form): void{
-  this.service.changeCurrentForm(form);
+  this.navigationService.changeCurrentForm(form);
 }
 
 ngOnInit(): void{
-  this.service.lastValidPage.subscribe(isValid => {this.isValid = isValid; });
-  this.service.currentForm.subscribe(currentForm => {this.currentForm = currentForm; });
+  this.router.events
+  .pipe(filter(event => event instanceof NavigationEnd))
+  .subscribe((event: NavigationEnd) => {
+    this.currentUrl = event.url;
+    const connectStream = combineLatest([this.navigationService.lastValidPage, this.navigationService.currentForm]);
+    const subscribe = connectStream.subscribe(([lastValidPage, currentForm]) => {
+    this.isValid = lastValidPage;
+    this.currentForm = currentForm;
+    if (this.currentUrl === '/home-page') {
+      this.currentPageData.pageTitle = 'Wybierz badanie';
+      this.isHidden = true;
+     }else if (this.currentUrl === '/test-form-page') {
+     this.isHidden = false;
+     this.currentPageData = this.navigationService.changeNavigationProperties(this.currentForm, this.isValid);
+     } else if (this.currentUrl === '/interpretation') {
+      this.isHidden = true;
+      this.currentPageData.pageTitle = 'Wybierz badanie ponownie';
+    }else { this.isHidden = false; }
+  });
+  });
 }
-
-ngDoCheck(): void{
-  if (this.currentRoute.url === '/home-page') {
-    this.pageTitle = 'Wybierz badanie';
-    this.isHidden = true;
-   }else if (this.currentRoute.url === '/test-form-page') {
-   this.isHidden = false;
-   switch (this.currentForm) {
-     case 'test-choice':
-       this.previousPath = 'home-page';
-       this.previousForm = null;
-       this.nextPath = 'test-form-page';
-       this.nextForm = 'gender-choice';
-       this.pageTitle = 'Wybierz płeć';
-       if (this.isValid === 'test'){
-         this.isEnabled = true;
-       } else { this.isEnabled = false; }
-       break;
-     case 'gender-choice':
-       this.previousPath = 'test-form-page';
-       this.previousForm = 'test-choice';
-       this.nextPath = 'test-form-page';
-       this.nextForm = 'entering-results';
-       this.pageTitle = 'Wpisz wyniki';
-       if (this.isValid === 'gender'){
-        this.isEnabled = true;
-      } else { this.isEnabled = false; }
-       break;
-      case 'entering-results':
-        this.previousPath = 'test-form-page';
-        this.previousForm = 'gender-choice';
-        this.nextPath = 'interpretation';
-        this.pageTitle = 'Interpretacja';
-        if ((this.isValid === 'lipids') || (this.isValid === 'thyroid')){
-        this.isEnabled = true;
-      } else { this.isEnabled = false; }
-        break; }
-   } else if (this.currentRoute.url === '/interpretation') {
-    this.isHidden = true;
-    this.pageTitle = 'Wybierz badanie ponownie';
-  }else { this.isHidden = false; }
- }
 }
