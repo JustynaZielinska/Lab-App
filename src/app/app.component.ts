d68a216af41ee11255e979e46d1c8fe4a2fbb96e
juzielska <juzielska@gmail.com>
2021-02-02 18:13:53 +0100

    Optimized routing animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,28 @@ import { trigger, transition, style, query, animate, group } from '@angular/anim
   animations: [
     trigger('routerAnimation', [
       transition('1 => 2, 2 => 3', [
-          query(':enter', style({ opacity: 0, transform: 'translateX(50%)' })),
-          query(':leave', style({ opacity: 1, transform: 'translateX(0)' })),
+          query(':enter', style({ transform: 'translateX(50%)', opacity: 0 })),
+          query(':leave', style({ transform: 'translateX(0)', opacity: 1 })),
           query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
           group([
               query(':leave', [
-                  animate('0.4s', style({ opacity: 0, transform: 'translateX(-50%)' })),
+                  animate('0.4s', style({ transform: 'translateX(-50%)', opacity: 0 })),
               ]),
               query(':enter', [
-                animate('0.4s', style({ opacity: 1, transform: 'translateX(0)' })),
+                animate('0.4s', style({ transform: 'translateX(0)', opacity: 1 })),
           ]),
       ]),
   ]),
   transition('2 => 1, 3 => 2', [
-    query(':enter', style({ opacity: 1, transform: 'translateX(-50%)' })),
-    query(':leave', style({ opacity: 0, transform: 'translateX(0)' })),
+    query(':enter', style({ transform: 'translateX(-50%)', opacity: 0 })),
+    query(':leave', style({ transform: 'translateX(0)', opacity: 1 })),
     query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
     group([
         query(':leave', [
-            animate('0.4s', style({ opacity: 1, transform: 'translateX(50%)' })),
+            animate('0.4s', style({ transform: 'translateX(50%)', opacity: 0 })),
         ]),
         query(':enter', [
-          animate('0.4s', style({ opacity: 0, transform: 'translateX(0)' })),
+          animate('0.4s', style({ transform: 'translateX(0)', opacity: 1 })),
     ]),
 ]),
 ])
